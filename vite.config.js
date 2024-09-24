ce00f3114cf436d45ddef9925fd1e414e2cf23f8
brainstormerjr <60263058+brainstormerjr@users.noreply.github.com>
2024-09-24 17:23:55 +0800

    Fixed 'patch.json' import issue - 'require' not defined still persists, will need to fix with post-build script

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+				// cf. https://rollupjs.org/configuration-options/#external
+				external: ['css-tree'],
+		}
+	}
 };
 
 export default config;
